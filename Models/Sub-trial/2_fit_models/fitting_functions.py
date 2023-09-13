d8311dbd76d48d33bf6552436d4f5c9770211f38
Inês Laranjeira <ines.laranjeira@research.fchampalimaud.org>
2023-09-13 17:29:53 +0100

    Moved functions from script to functions file

--- Models/Sub-trial/2_fit_models/fitting_functions.py
+++ Models/Sub-trial/2_fit_models/fitting_functions.py
@@ -114,3 +114,158 @@ def softmax(x, class_num, num_timesteps):
         pi[i] = np.exp(x[:, i]) / np.sum(np.exp(x[:, :]), axis=1)
     return pi
 
+
+################################################### INES's FUNCTIONS ############################################################
+
+def reduce_design_matrix (init, end, event_type_list, session_trials, design_matrix, test_set, most_likely_states, multiplier):
+    
+    for e, this_event in enumerate(event_type_list):
+        
+        # Initialize variables
+        test_length = np.shape(test_set)[0]
+        reduced_design_matrix = design_matrix[:test_length*2].append(design_matrix[-test_length*2:])
+        reduced_design_matrix['most_likely_states'] = most_likely_states
+        reduced_design_matrix['new_bin'] = reduced_design_matrix['Bin'] * 0
+        reduced_design_matrix['correct'] = reduced_design_matrix['Bin'] * 0
+        reduced_design_matrix['choice'] = reduced_design_matrix['Bin'] * 0
+                
+        events = session_trials[this_event]
+        feedback = session_trials['feedbackType']
+        choice = session_trials['choice']
+        
+        events = session_trials[this_event]
+        #state_stack = np.zeros((len(events), end + init)) * np.nan
+                
+        for t, trial in enumerate(events[:-2]):
+            event = events[t]
+            
+            # Check feedback
+            if feedback[t] ==1:
+                reduced_design_matrix.loc[(reduced_design_matrix['Bin']<= event*multiplier + end) &
+                                            (reduced_design_matrix['Bin']> event*multiplier + init), 
+                                            'correct'] = 1
+            elif feedback[t] == 0:
+                reduced_design_matrix.loc[(reduced_design_matrix['Bin']<= event*multiplier + end) &
+                                            (reduced_design_matrix['Bin']> event*multiplier + init), 
+                                            'correct'] = 0
+            # Check choice
+            if choice[t] ==1:
+                reduced_design_matrix.loc[(reduced_design_matrix['Bin']<= event*multiplier + end) &
+                                            (reduced_design_matrix['Bin']> event*multiplier + init), 
+                                            'choice'] = 'right'
+            elif choice[t] == -1:
+                reduced_design_matrix.loc[(reduced_design_matrix['Bin']<= event*multiplier + end) &
+                                            (reduced_design_matrix['Bin']> event*multiplier + init), 
+                                            'choice'] = 'left'
+            # Rename bins so that they are aligned on stimulus onset
+            if event > 0:
+                event_window = reduced_design_matrix.loc[(reduced_design_matrix['Bin']<= event*multiplier + end) & (reduced_design_matrix['Bin']> event*multiplier + init)]
+                onset_bin = reduced_design_matrix.loc[reduced_design_matrix['Bin']>= event*multiplier, 'Bin']
+                if (len(event_window)>0): # & len(onset_bin)>0:
+                    bin = list(onset_bin)[0]
+                    reduced_design_matrix.loc[(reduced_design_matrix['Bin']<= event*multiplier + end) &
+                                            (reduced_design_matrix['Bin']> event*multiplier + init), 
+                                            'new_bin'] = reduced_design_matrix.loc[(reduced_design_matrix['Bin']< event*multiplier + end) & 
+                                            (reduced_design_matrix['Bin']>= event*multiplier + init), 'Bin'] - bin
+                else:
+                    reduced_design_matrix.loc[(reduced_design_matrix['Bin']<= event*multiplier + end) & (reduced_design_matrix['Bin']> event*multiplier + init), 'new_bin'] = np.nan
+            else:
+                reduced_design_matrix.loc[(reduced_design_matrix['Bin']<= event*multiplier + end) & (reduced_design_matrix['Bin']> event*multiplier + init), 'new_bin'] = np.nan
+                
+    return reduced_design_matrix
+                
+                
+def plot_states_aligned(init, end, reduced_design_matrix, event_type_name, bin_size):
+
+    for e, this_event in enumerate(event_type_name):
+            
+        # PLOT
+        fig, ax = plt.subplots(nrows=2, ncols=2, sharex=False, sharey=False, figsize=[12, 7])
+        plt.rc('font', size=12)
+        use_data = reduced_design_matrix.dropna()
+        use_data['new_bin'] = use_data['new_bin'] * bin_size
+        #use_data = use_data[:test_length*2]
+        
+        # Correct left
+        a = sns.histplot(x='new_bin', hue='most_likely_states', data=use_data.loc[(use_data['correct']==1) 
+                                                                                & (use_data['choice']=='left')], stat='count', 
+                        multiple="stack", binwidth=bin_size, binrange=(bin_size*init, bin_size*end), legend=False, ax = ax[0, 0])
+        # Correct right
+        b = sns.histplot(x='new_bin', hue='most_likely_states', data=use_data.loc[(use_data['correct']==1) 
+                                                                                & (use_data['choice']=='right')], stat='count', 
+                        multiple="stack", binwidth=bin_size, binrange=(bin_size*init, bin_size*end), legend=False, ax = ax[0, 1])
+        # Incorrect left
+        c = sns.histplot(x='new_bin', hue='most_likely_states', data=use_data.loc[(use_data['correct']==0) 
+                                                                                & (use_data['choice']=='left')], stat='count',
+                        multiple="stack", binwidth=bin_size, binrange=(bin_size*init, bin_size*end), legend=False, ax = ax[1, 0])
+        # Incorrect right
+        d = sns.histplot(x='new_bin', hue='most_likely_states', data=use_data.loc[(use_data['correct']==0) 
+                                                                                & (use_data['choice']=='right')], 
+                        stat='count', multiple="stack", binwidth=bin_size, binrange=(bin_size*init, bin_size*end), legend=False, ax = ax[1, 1])
+        
+        ax[0, 0].set_title(str(this_event + ' - correct left'))
+        ax[0, 0].set_xlabel(str('Time from event (s)'))
+
+        ax[0, 1].set_title(str(this_event + ' - correct right'))
+        ax[0, 1].set_xlabel(str('Time from event (s)'))
+        
+        ax[1, 0].set_title(str(this_event + ' - incorrect left'))
+        ax[1, 0].set_xlabel(str('Time from event (s)'))
+        
+        ax[1, 1].set_title(str(this_event + ' - incorrect right'))
+        ax[1, 1].set_xlabel(str('Time from event (s)'))
+        
+        plt.tight_layout()
+        plt.show()
+    
+
+def params_to_df (var_names, num_states, num_train_batches, fit_params, norm_results):
+
+    learned_params = pd.DataFrame(columns=['fold', 'state', 'variable'])  # , index=np.arange(0, (num_states*num_train_batches), 1)
+
+    for v, var in enumerate(var_names):
+        for s, state in enumerate(range(num_states)):
+            df = pd.DataFrame(columns=['fold', 'state', 'variable'])
+            values = fit_params[2].means[:].T[v][s, :]
+            
+            # Normalize results 
+            if norm_results == True:
+                # Calculate min and max values
+                min_value = np.min(values)
+                max_value = np.max(values)
+                # Normalize
+                values = (values - min_value) / (max_value - min_value) * 2 - 1
+                
+            df['value'] = values
+            df['fold'] = np.arange(1, num_train_batches+1, 1)
+            df['variable'] = var
+            df['state'] = state
+            learned_params = learned_params.append(df)
+            
+    return learned_params
+
+
+def plot_transition_mat (init_params, fit_params):
+    
+    plt.title('Initial parameters')
+    plot_transition_matrix(init_params[1].transition_matrix)
+    init_params[1].transition_matrix
+
+    plt.title('Fold 1')
+    plot_transition_matrix(fit_params[1].transition_matrix[0])
+    fit_params[1].transition_matrix[0]
+    plt.title('Fold 2')
+    plot_transition_matrix(fit_params[1].transition_matrix[1])
+    fit_params[1].transition_matrix[1]
+    plt.title('Fold 3')
+    plot_transition_matrix(fit_params[1].transition_matrix[2])
+    fit_params[1].transition_matrix[2]
+    
+    
+#def split_train_test_validation ():
+    
+    
+    
+#def compute_model_perf_metrics ():
+    
+    
