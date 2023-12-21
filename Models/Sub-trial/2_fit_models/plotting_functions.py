56a44886bdcb7a3256278b27bac3732c87191edf
Inês Laranjeira <ines.laranjeira@research.fchampalimaud.org>
2023-12-21 13:20:13 +0000

    various changes

--- Models/Sub-trial/2_fit_models/plotting_functions.py
+++ Models/Sub-trial/2_fit_models/plotting_functions.py
@@ -14,7 +14,7 @@ from sklearn.preprocessing import StandardScaler, MinMaxScaler
 
 # Custom functions
 functions_path =  '/home/ines/repositories/representation_learning_variability/Functions/'
-functions_path = '/Users/ineslaranjeira/Documents/Repositories/representation_learning_variability/Functions/'
+#functions_path = '/Users/ineslaranjeira/Documents/Repositories/representation_learning_variability/Functions/'
 os.chdir(functions_path)
 from one_functions_generic import prepro
 
