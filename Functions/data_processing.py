9a3852d1547871874a08a98af5b19cb4e726a45b
Inês <ines.laranjeira@research.fchampalimaud.org>
2024-04-29 23:27:11 +0100

    Added more variables to plot with wheel movements

--- Functions/data_processing.py
+++ Functions/data_processing.py
@@ -19,7 +19,7 @@ from scipy.interpolate import interp1d
 
 # Custom functions
 functions_path =  '/home/ines/repositories/representation_learning_variability/Functions/'
-# functions_path = '/Users/ineslaranjeira/Documents/Repositories/representation_learning_variability/Functions/'
+functions_path = '/Users/ineslaranjeira/Documents/Repositories/representation_learning_variability/Functions/'
 os.chdir(functions_path)
 from one_functions_generic import prepro
 
