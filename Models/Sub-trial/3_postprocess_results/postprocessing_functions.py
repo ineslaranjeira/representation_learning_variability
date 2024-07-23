d16aa5365924a0cb0945cceda0fb617406be0004
Inês Laranjeira <ines.laranjeira@research.fchampalimaud.org>
2024-07-23 16:18:49 +0100

    moved functions from scripts to the function file

--- Models/Sub-trial/3_postprocess_results/postprocessing_functions.py
+++ Models/Sub-trial/3_postprocess_results/postprocessing_functions.py
@@ -2,10 +2,12 @@
 IMPORTS
 """
 import autograd.numpy as np
-import pickle
 import pandas as pd
 from collections import defaultdict
 from sklearn.preprocessing import StandardScaler, Normalizer
+import seaborn as sns
+import matplotlib.pyplot as plt
+
 
 def best_lag_kappa(all_lls, all_baseline_lls, design_matrix, num_train_batches, kappas, Lags):
     
@@ -139,3 +141,137 @@ def remove_states_flt(most_likely_states, threshold):
 
     
     return new_states
+
+
+def get_index(var_timeseries, target_value):
+
+    # Assume we find an instance where x is close to 5
+    index = (np.abs(var_timeseries - target_value)).argmin()
+
+    return index
+
+
+def update_var(target_value, var_timeseries, var_name, mouse_dynamics, state):
+    
+    weights = mouse_dynamics[var_name]['weights'][state][0]
+    lag_num = np.shape(mouse_dynamics[var_name]['weights'][state])[1]
+
+    # Start with input variable value
+    updated_var = 0
+    closest_to_target = get_index(var_timeseries, target_value)
+    # Loop through lags to complete update
+    for l, lag in enumerate(range(lag_num)):
+        # Weight for that lag
+        lag_weight = weights[l]  
+        # Get variable with corresponding lag
+        if l == 0:
+            use_var = target_value
+        else:
+            use_var = np.array(var_timeseries)[closest_to_target-l]
+        updated_var += use_var * lag_weight
+        
+    return updated_var
+
+
+def plot_trajectories(new_states, inverted_mapping, design_matrix_heading, x_var, y_var, axs, trajectory_num):
+    
+    state_switches = np.diff(new_states)
+    switch_idxs = np.where(np.abs(state_switches)>0)[0][0:-1] 
+    switch_types = np.unique(new_states)
+    switch_types = switch_types[~np.isnan(switch_types)]
+    switch_type_idx = new_states[switch_idxs+1]
+
+    for t, type in enumerate(switch_types):
+        switch_interest = switch_idxs[np.where(switch_type_idx==type)]
+        switch_plot = np.random.choice(switch_interest, trajectory_num)
+        
+        # Loop through switche types
+        for s, switch in enumerate(switch_plot):
+            trajectory_end = switch_idxs[switch_idxs>switch][0]
+            
+            
+            # Check if trajectory is long enough, if not, draw new switch
+            while (trajectory_end - switch) < 10:
+                switch = np.random.choice(switch_interest, 1)[0]
+                # print(switch, switch_idxs, switch_idxs[switch_idxs>switch])
+                trajectory_end = switch_idxs[switch_idxs>switch][0]
+            
+            current_state = new_states[switch+1]
+            state_label = inverted_mapping[current_state]
+            
+            state_wheel = int(state_label[0])
+            state_whisker = int(state_label[2])
+            ax = axs[state_wheel, state_whisker]
+
+            color = sns.color_palette("viridis", len(switch_types))[int(current_state)]
+            label = state_label
+            xx = design_matrix_heading[x_var][switch+1:trajectory_end]
+            yy = design_matrix_heading[y_var][switch+1:trajectory_end]
+
+            if s == trajectory_num -1:
+                ax.plot(xx, yy, alpha=0.5, color=color, label=label)
+                ax.legend()
+            else:
+                ax.plot(xx, yy, alpha=0.5, color=color)
+
+            ax.set_xlabel('Wheel velocity - state ' + str(state_wheel))
+            ax.set_ylabel('Whisker motion energy - state ' + str(state_whisker))
+            
+            
+def plot_x_y_dynamics(x_var, y_var, mouse_dynamics, new_states, design_matrix_heading, inverted_mapping, grid_density, trajectory_num, plot_traj=True):
+    
+    fig, axs = plt.subplots(2, 2, figsize=(10, 9))
+    unique_states = np.array(list(inverted_mapping.keys()))
+    unique_states = unique_states[~np.isnan(unique_states)]
+
+    for s, state in enumerate(unique_states):
+        
+        x = np.linspace(np.min(design_matrix_heading[x_var]), 
+                    np.max(design_matrix_heading[x_var]), grid_density)
+        y = np.linspace(np.min(design_matrix_heading[y_var]), 
+                        np.max(design_matrix_heading[y_var]), grid_density)
+        X, Y = np.meshgrid(x, y)
+        U = np.zeros_like(X)
+        V = np.zeros_like(Y)
+
+        where = np.where(np.array(list(inverted_mapping.keys())) == state)[0][0]
+        state_wheel = int(inverted_mapping[where][0])
+        state_whisker = int(inverted_mapping[where][2])
+        ax = axs[state_wheel, state_whisker]
+
+        for i in range(len(x)):
+            for j in range(len(y)):
+                U[i, j] = X[j, i] - update_var(X[j, i], design_matrix_heading[x_var], x_var, mouse_dynamics, 
+                                    state_wheel)
+                V[i, j] = Y[j, i] - update_var(Y[j, i], design_matrix_heading[y_var], y_var, mouse_dynamics, 
+                                    state_whisker)
+
+        # xy': Arrow direction in data coordinates, i.e. the arrows point from (x, y) to (x+u, y+v).     
+        ax.quiver(X, Y, U, V, angles='xy')
+        ax.set_xlabel('Wheel velocity - state ' + str(state_wheel))
+        ax.set_ylabel('Whisker motion energy - state ' + str(state_whisker))
+
+        ax.set_ylim([-1.5, 4])
+        ax.set_xlim([-4, 4])
+        
+    if plot_traj == True:
+        plot_trajectories(new_states, inverted_mapping, design_matrix_heading, x_var, y_var, axs, trajectory_num)
+        
+    plt.tight_layout()
+    plt.show()
+    
+    
+def transition_probabilities(states, unique_states):
+    
+    transition_matrix = np.zeros((len(unique_states), len(unique_states))) * np.nan
+    previous = states[:-1]
+    current = states[1:]
+    for s, st in enumerate(unique_states):
+        for s_p, s_prev in enumerate(unique_states):
+            interest_current = np.where(current==st)[0]
+            interest_prev = np.where(previous==s_prev)[0]
+            joint = len(np.intersect1d(interest_current, interest_prev))/ len(previous)
+            marginal = len(interest_prev) / len(previous)
+            transition_matrix[s_p, s] = joint / marginal
+        
+    return transition_matrix
