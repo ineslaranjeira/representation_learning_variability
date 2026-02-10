8147382c215af0cc8b7bed2a68367c0e31ac15c5
Inês <ines.laranjeira@research.fchampalimaud.org>
2026-02-10 11:29:24 +0000

    Updates and file reorganization

---
+++ paper-individuality/decoding/functions.py
@@ -0,0 +1,115 @@
+
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap, to_rgb, to_hex
+
+"""
+SCRIPT 
+"""
+def idxs_from_files(design_matrices):
+    
+    idxs = []
+    mouse_names = []
+    for m, mat in enumerate(design_matrices):
+        mouse_name = design_matrices[m][51:]
+        eid = design_matrices[m][14:50]
+        idx = str(eid + '_' + mouse_name)
+
+        if len(idxs) == 0:
+            idxs = idx
+            mouse_names = mouse_name
+        else:
+            idxs = np.hstack((idxs, idx))
+            mouse_names = np.hstack((mouse_names, mouse_name))
+            
+    return idxs, mouse_names
+
+
+def create_grouped_gradient_palette(n_groups=4, shades_per_group=4, base_palette='tab10'):
+    # Pick n_groups distinct colors from base palette
+    base_colors = sns.color_palette(base_palette, n_colors=n_groups)
+    
+    # Function to generate shades for a given color
+    def generate_shades(color, n_shades):
+        color_rgb = np.array(to_rgb(color))
+        # Create shades by interpolating towards white (lighter) or black (darker)
+        factors = np.linspace(0.4, 1.0, n_shades)  # You can adjust range for more/less contrast
+        return [to_hex(color_rgb * factor + (1 - factor)) for factor in factors]
+    
+    # Build full palette
+    full_palette = []
+    for color in base_colors:
+        shades = generate_shades(color, shades_per_group)
+        full_palette.extend(shades)
+    
+    return ListedColormap(full_palette)
+
+
+def plot_cm(decoding_result, trial_epochs, size, control=False):
+    """
+    PLOT RESULTS
+    """
+
+    # -- Confusion Matrix
+    # labels = trial_epochs
+
+    # Results on original model
+    plt.rc('font', size=9) 
+    plt.figure(figsize=size)
+    # hmap = sns.color_palette("mako", as_cmap=True)
+    hmap=plt.cm.get_cmap('Greys')
+    data = decoding_result.loc[decoding_result['shuffle'] == 0]
+    sns.heatmap(data['confusion_matrix'].mean(), annot=False, square=True,
+        yticklabels=trial_epochs, xticklabels=trial_epochs, 
+        cmap= hmap, vmin=0, vmax=1, fmt=".2f") 
+
+    # plt.xticks([.5, 1.5, 2.5, 3.5], trial_epochs)
+    # plt.yticks([.5, 1.5, 2.5, 3.5], trial_epochs)
+    plt.xticks(rotation = 90)
+    plt.yticks(rotation = 0)
+    plt.xlabel('Predicted label')
+    plt.ylabel('True label')
+    # plt.savefig('full_cm.svg',dpi=500)
+    plt.show()
+    print('F1 results', data['f1'].mean())
+
+    if control:
+        # Results from shuffled model
+        plt.rc('font', size=9) 
+        plt.figure(figsize=size)
+        data = decoding_result.loc[decoding_result['shuffle'] >0]
+        sns.heatmap(data['confusion_matrix'].mean(), annot=False, square=True,
+            yticklabels=trial_epochs, xticklabels=trial_epochs, 
+            cmap= hmap, vmin=0, vmax=1, fmt=".2f")
+
+        # plt.xticks([.5, 1.5, 2.5, 3.5], trial_epochs)
+        # plt.yticks([.5, 1.5, 2.5, 3.5], trial_epochs)
+        plt.xticks(rotation = 90)
+        plt.yticks(rotation = 0)
+        plt.xlabel('Predicted label')
+        plt.ylabel('True label')
+        plt.show()
+        print('F1 shuffled results',  data['f1'].mean())
+    plt.tight_layout()
+
+
+def plot_f1(decoding_result):
+    # -- F1 score per model, original and shuffled
+    data = decoding_result.copy()
+    data['f1'] = data['f1'].astype(float)
+
+    data.loc[data['shuffle'] >= 1, 'shuffle'] = 'Shuffled'
+    data.loc[data['shuffle'] == 0, 'shuffle'] = 'Original'
+    data = data.rename(columns={'shuffle': 'Dataset'})
+
+    plt.rc('font', size=12) 
+    plt.figure(figsize=[4.5, 4])
+    sns.boxplot(y='f1', x='Dataset', data=data, color='grey') 
+    sns.swarmplot(y='f1', x='Dataset', data=data, color='black', dodge=True, alpha=0.7, size=3)
+    plt.ylim([0,1])
+    plt.ylabel('Accuracy score (F1)')
+    plt.legend(bbox_to_anchor=(1.05, 1))
+    sns.despine(top=True, right=True)
+    #plt.savefig('violin.svg',dpi=500)
+    
